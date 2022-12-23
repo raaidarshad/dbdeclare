cebce0f89128896dd45a492cffecf35b9a4ebcdd
Raaid <raaid@protonmail.com>
2022-12-23 14:14:01 -0500

    add first pass of grant_all and run_all to Base

--- postgres_declare/base.py
+++ postgres_declare/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Engine
 
 from postgres_declare.entities.entity import Entity
+from postgres_declare.grant import Grantable
 
 
 class Base:
@@ -10,6 +11,20 @@ class Base:
             Entity._engine = engine
         for entity in Entity.entities:
             entity._safe_create()
+
+    @classmethod
+    def grant_all(cls, engine: Engine | None = None) -> None:
+        if engine:
+            Entity._engine = engine
+        for entity in Entity.entities:
+            if isinstance(entity, Grantable):
+                entity._grant()
+
+    @classmethod
+    def run_all(cls, engine: Engine) -> None:
+        Entity._engine = engine
+        cls.create_all()
+        cls.grant_all()
 
     @classmethod
     def remove_all(cls, engine: Engine | None = None) -> None:
