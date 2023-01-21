41f0ceab240b05395ad14c4f1b2e65871ff69750
Raaid <raaid@protonmail.com>
2023-01-21 12:57:59 -0500

    change database entities to accept ONE database, or else a lot of unique things break later

--- postgres_declare/entities/database_content.py
+++ postgres_declare/entities/database_content.py
@@ -14,35 +14,28 @@ class DatabaseContent(DatabaseEntity):
         self,
         name: str,
         sqlalchemy_base: Type[DeclarativeBase],
-        databases: Sequence[Database],
+        database: Database,
         schemas: Sequence[Schema] | None = None,
         depends_on: Sequence[Entity] | None = None,
         check_if_exists: bool | None = None,
     ):
-        super().__init__(name=name, depends_on=depends_on, databases=databases, check_if_exists=check_if_exists)
+        super().__init__(name=name, depends_on=depends_on, database=database, check_if_exists=check_if_exists)
         self.base = sqlalchemy_base
         # schemas doesn't do anything since __table_args__ in sqlalchemy defines the schema
         # BUT it helps to have it as a dependency here to remind the user to make schemas they intend to use
         self.schemas = schemas
 
     def _create(self) -> None:
-        for db in self.databases:
-            self.base.metadata.create_all(db.db_engine())
+        self.base.metadata.create_all(self.database.db_engine())
 
     def _exists(self) -> bool:
-        tables_in_db = []
-        for db in self.databases:
-            inspector: Inspector = inspect(db.db_engine())
-            tables_in_db.append(
-                all(
-                    [
-                        inspector.has_table(table_name=table.name, schema=table.schema)
-                        for table in self.base.metadata.tables.values()
-                    ]
-                )
-            )
-        return all(tables_in_db)
+        inspector: Inspector = inspect(self.database.db_engine())
+        return all(
+            [
+                inspector.has_table(table_name=table.name, schema=table.schema)
+                for table in self.base.metadata.tables.values()
+            ]
+        )
 
     def _drop(self) -> None:
-        for db in self.databases:
-            self.base.metadata.drop_all(db.db_engine())
+        self.base.metadata.drop_all(self.database.db_engine())
--- postgres_declare/entities/database_entity.py
+++ postgres_declare/entities/database_entity.py
@@ -9,25 +9,22 @@ class DatabaseEntity(Entity):
     def __init__(
         self,
         name: str,
-        databases: Sequence[Database],
+        database: Database,
         depends_on: Sequence[Entity] | None = None,
         check_if_exists: bool | None = None,
     ):
-        self.databases: Sequence[Database] = databases
+        self.database = database
         super().__init__(name=name, depends_on=depends_on, check_if_exists=check_if_exists)
 
 
 class DatabaseSqlEntity(SQLMixin, DatabaseEntity):
     # TODO maybe inherit from grantable, maybe do it per entity?
     def _create(self) -> None:
-        for db in self.databases:
-            self._commit_sql(engine=db.db_engine(), statements=self._create_statements())
+        self._commit_sql(engine=self.database.db_engine(), statements=self._create_statements())
 
     def _exists(self) -> bool:
-        return all(
-            [self._fetch_sql(engine=db.db_engine(), statement=self._exists_statement())[0][0] for db in self.databases]
-        )
+        rows = self._fetch_sql(engine=self.database.db_engine(), statement=self._exists_statement())
+        return rows[0][0]  # type: ignore
 
     def _drop(self) -> None:
-        for db in self.databases:
-            self._commit_sql(engine=db.db_engine(), statements=self._drop_statements())
+        self._commit_sql(engine=self.database.db_engine(), statements=self._drop_statements())
