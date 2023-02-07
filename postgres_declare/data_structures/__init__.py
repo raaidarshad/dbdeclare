1bc3460973f7f90f31a0b26424735ca6741e21df
Raaid <raaid@protonmail.com>
2023-02-06 21:00:25 -0500

    Organize imports (#12)
    * import public entities to entity init file, and make this okay for flake8
    
    * import public structures to data_structures' init file
    
    * fix up inits to comply with mypy, run some package updates so lockfile changed, add py.typed

---
+++ postgres_declare/data_structures/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["GrantOn", "GrantTo", "Privilege"]
+
+from postgres_declare.data_structures.grant_on import GrantOn
+from postgres_declare.data_structures.grant_to import GrantTo
+from postgres_declare.data_structures.privileges import Privilege
