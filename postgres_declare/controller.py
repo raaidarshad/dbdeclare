495f01305ebf343a3ec65230f392595b14bcf011
Raaid <raaid@protonmail.com>
2023-02-08 15:49:38 -0500

    add docstring to Controller.create_all and add a static method to handle engine assignment for all Controller methods

--- postgres_declare/controller.py
+++ postgres_declare/controller.py
@@ -12,62 +12,62 @@ class Controller:
 
     @classmethod
     def create_all(cls, engine: Engine | None = None) -> None:
-        if engine:
-            Entity._engine = engine
+        """
+        Attempts to create all defined entities.
+        :param engine: A :class:`sqlalchemy.Engine` that defines the connection to a Postgres instance/cluster.
+        """
+        cls._handle_engine(engine)
         for entity in Entity.entities:
             entity._safe_create()
 
     @classmethod
     def grant_all(cls, engine: Engine | None = None) -> None:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         for entity in Entity.entities:
             if isinstance(entity, Role):
                 entity._safe_grant()
 
     @classmethod
     def run_all(cls, engine: Engine | None = None) -> None:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         cls.create_all()
         cls.grant_all()
 
     @classmethod
     def drop_all(cls, engine: Engine | None = None) -> None:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         for entity in reversed(Entity.entities):
             entity._safe_drop()
 
     @classmethod
     def revoke_all(cls, engine: Engine | None = None) -> None:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         for entity in reversed(Entity.entities):
             if isinstance(entity, Role):
                 entity._safe_revoke()
 
     @classmethod
     def remove_all(cls, engine: Engine | None = None) -> None:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         cls.revoke_all()
         cls.drop_all()
 
     @classmethod
     def _all_entities_exist(cls, engine: Engine | None = None) -> bool:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         return all([entity._exists() for entity in Entity.entities])
 
     @classmethod
     def _all_grants_exist(cls, engine: Engine | None = None) -> bool:
-        if engine:
-            Entity._engine = engine
+        cls._handle_engine(engine)
         return all([role._grants_exist() for role in Entity.entities if isinstance(role, Role)])
 
     @classmethod
     def _all_exist(cls, engine: Engine | None = None) -> bool:
+        cls._handle_engine(engine)
+        return cls._all_entities_exist() and cls._all_grants_exist()
+
+    @staticmethod
+    def _handle_engine(engine: Engine | None = None) -> None:
         if engine:
             Entity._engine = engine
-        return cls._all_entities_exist() and cls._all_grants_exist()
